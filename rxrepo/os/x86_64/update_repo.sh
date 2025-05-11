55daa0859b070bc27cbe2a9958d5db185a5282f9
rXelelo <rXelelo@outlook.com>
2025-05-11 14:15:42 +0300

    hello

---
+++ rxrepo/os/x86_64/update_repo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm rxrepo*
+
+echo "repo-add"
+repo-add -n -R rxrepo.db.tar.gz *.pkg.tar.zst
+
+sleep 1
+
+rm rxrepo.db
+
+rm rxrepo.files
+
+mv rxrepo.db.tar.gz rxrepo.db
+
+mv rxrepo.files.tar.gz rxrepo.files
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
